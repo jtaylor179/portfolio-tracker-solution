d39f4c3e912d125818dfea0983faeee0d7a0f4bb
Jeff Taylor <jefftaylor@Jeffs-MacBook-Pro.local>
2023-04-15 23:45:26 -0400

    init

---
+++ Portfolio.cs
@@ -0,0 +1,7 @@
+namespace StockPortfolioApi {
+    public class Portfolio {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Stocks { get; set; }
+    }
+}
